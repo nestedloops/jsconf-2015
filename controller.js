4c140e0d9e7ca167c394c263f48eafa2e1173725
janmonschke <jan.monschke@soundcloud.com>
2015-08-02 16:21:31 +0200

    add first MIDI draft

--- controller.js
+++ controller.js
@@ -1,7 +1,23 @@
 const EventEmitter = require('events').EventEmitter;
 
+const COLOR_CODES = {
+  OFF: 12,
+  RED: 15,
+  AMBER: 63,
+  YELLOW: 62,
+  GREEN: 60
+};
+
+const midi = require('web-midi');
+midi.getPortNames((err, names) => {
+  console.log('sdf', err, names);
+  if(names.length === 0) return;
+  const l = global.l = midi(names[0]);
+  l.write([144, 0, COLOR_CODES.RED]);
+});
+
 class Controller extends EventEmitter {
 
 }
 
-modules.exports = Controller;
+module.exports = Controller;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Scheduler = require('./scheduler');
 const Clip = require('./clip');
 const c = global.c = new Clip('/samples/beat1-100bpm.wav');
+require('./controller')
+/*
 c.load().then(() => {
   c.touch();
   const s = new Scheduler([c]);
   s.start();
 });
+*/
