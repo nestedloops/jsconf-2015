c3da709df93579a92bed794f2d9e6ec80f1b0940
janmonschke <jan.monschke@soundcloud.com>
2015-08-03 21:47:39 +0200

    more samples

--- mappings.js
+++ mappings.js
@@ -1,5 +1,7 @@
 const Clip = require('./clip');
 
 module.exports = {
-  0: new Clip('/samples/beat1-100bpm.wav')
+  0: new Clip({location: '/samples/beat1-100bpm-2bars.wav', bars: 2}),
+  16: new Clip({location: '/samples/beatbox-100bpm-4bars.wav', bars: 4}),
+  32: new Clip({location: '/samples/loop-100bpm-2bars.wav', bars: 2})
 };
