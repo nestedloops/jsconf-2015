e3062eec7f7b24b04ccca28d61215bfcfac9247f
Jan Monschke <jan.monschke@gmail.com>
2015-08-25 22:08:53 +0200

    watch files on start

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,8 @@
 var nodeStatic = require('node-static');
-
+var exec = require('child_process').exec;
 var fileServer = new nodeStatic.Server('.');
+
+exec('watchify index.js -o bundle.js -t babelify');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
